efe1c31872075bb9f669dd40a3de55e58cce6dd6
ystan98 <66168700+ystan98@users.noreply.github.com>
2022-01-10 21:06:06 +0800

    s

--- js/skeleton.js
+++ js/skeleton.js
@@ -11,7 +11,7 @@ main.component("nav-bar", {
         return {
             my_href: location.href,
             menu: [{
-                    href: "index.html",
+                    href: "index",
                     text: "About",
                 },
                 {
@@ -32,7 +32,7 @@ main.component("nav-bar", {
     template: `
     <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand"  href="#page-top"> Yee Sen</a>
+        <a class="navbar-brand"  href="index"> Yee Sen</a>
         <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <i class="fas fa-bars"></i>
         </button>
